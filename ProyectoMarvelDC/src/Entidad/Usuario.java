ae88555fd71604844f251505b2446386c48ca02e
MayerlyAgutierrez <99781925+MayerlyAgutierrez@users.noreply.github.com>
2022-03-17 23:56:36 -0500

    Se modificó la clase usuario

--- ProyectoMarvelDC/src/Entidad/Usuario.java
+++ ProyectoMarvelDC/src/Entidad/Usuario.java
@@ -1,9 +1,35 @@
 package Entidad;
 
+import java.io.File;
+
 public class Usuario {
     private int iduser;
     private String nameuser;
     private String password;
+    private String idurl;
+
+
+
+    public String getIdurl() {
+        return idurl;
+    }
+    public void setIdurl(String idurl) {
+        this.idurl = idurl;
+    }
+    public File getFileurl() {
+        return fileurl;
+    }
+    public void setFileurl(File fileurl) {
+        this.fileurl = fileurl;
+    }
+    public byte getStringtobye() {
+        return stringtobye;
+    }
+    public void setStringtobye(byte stringtobye) {
+        this.stringtobye = stringtobye;
+    }
+    private File fileurl;
+    private byte stringtobye;
 
     public Usuario() {
  
@@ -32,9 +58,27 @@ public class Usuario {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    
+    public Usuario(int iduser, String nameuser, String idurl, File fileurl, byte stringtobye) {
+        this.iduser = iduser;
+        this.nameuser = nameuser;
+        this.idurl = idurl;
+        this.fileurl = fileurl;
+        this.stringtobye = stringtobye;
+    }
+    public Usuario(int iduser, String nameuser, String password, String idurl, File fileurl, byte stringtobye) {
+        this.iduser = iduser;
+        this.nameuser = nameuser;
+        this.password = password;
+        this.idurl = idurl;
+        this.fileurl = fileurl;
+        this.stringtobye = stringtobye;
+    }
     @Override
     public String toString() {
         return "Usuario id=" + iduser + ", Nombre Usuario=" + nameuser + ", Contrasena=" + password ;
     }
     
 }
+
