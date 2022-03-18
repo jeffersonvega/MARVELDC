0b87c6d330d5c02046206b33e29a118704a23fef
MichaelUG <99856949+MichaelUG@users.noreply.github.com>
2022-03-17 23:59:31 -0500

    Se modifico el iUserDAO

--- ProyectoMarvelDC/src/Conexion/iUserDAO.java
+++ ProyectoMarvelDC/src/Conexion/iUserDAO.java
@@ -11,12 +11,9 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public interface iUserDAO {
-    void insertar(Usuario  cl)throws SQLException;
-    void actualizar(Usuario cl)throws SQLException;
-    void eliminar(int codigo)throws SQLException;
-    public int  traerId (Usuario id) throws SQLException;
+
     public ArrayList <Usuario> consultartodo()throws SQLException;
     void validarlogin(String name, String password)throws SQLException;
-    void validarloginfx(TextField txtUser, PasswordField txtPassword)throws SQLException;
+    
     
 }
