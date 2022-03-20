57e370c64b354615cfe9a1289a9d180c302a1155
jeffersonvega <jefferson.vega@outlook.es>
2022-03-19 22:20:21 -0500

    creacion de la interfaz del HeroeDAO

---
+++ ProyectoMarvelDC/src/Conexion/iHeroesDAO.java
@@ -0,0 +1,23 @@
+package Conexion;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.postgresql.core.SqlCommand;
+
+import Entidad.Heroes;
+
+public interface iHeroesDAO {
+    //void insertar(Heroes  heroes)throws SQLException, FileNotFoundException;
+    void insertarDatos(Heroes heroes) throws SQLException, FileNotFoundException, IOException;
+    void actualizar(Heroes heroes)throws SQLException, FileNotFoundException, IOException;
+    void eliminar(int codigo)throws SQLException;
+    public List <Heroes> consultartodo()throws SQLException;
+    public List <Heroes> consultarfiltro(String name)throws SQLException;
+    //public Heroes consultarheroe(Heroes heroe)throws SQLException;
+    
+
+}
